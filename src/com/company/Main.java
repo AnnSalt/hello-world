c358282458b946b02e4290aae5ff11a4ac0a1a1f
AnnSalt <saltykova111@mail.ru>
2021-11-26 19:24:41 +0300

    hello world added

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //System.out.println("a > b = " + (a > b));
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world +"!");
+
+        // write your code here
+    }
+}
